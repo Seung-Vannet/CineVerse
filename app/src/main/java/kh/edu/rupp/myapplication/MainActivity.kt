d4dc2be84a244cb2e12a103ba12343116a4d3029
macbook <seungvannet8@gmail.com>
2026-05-31 10:00:57 +0700

    Initial commit for Cinescope

---
+++ app/src/main/java/kh/edu/rupp/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package kh.edu.rupp.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import kh.edu.rupp.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/kh/edu/rupp/myapplication/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package kh.edu.rupp.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val CineBackground = Color(0xFF0F1014)
+val CinePrimary = Color(0xFFD81F26)
+val CineSurface = Color(0xFF1A1C22)
+val CineTextPrimary = Color(0xFFFFFFFF)
+val CineTextSecondary = Color(0xFF9CA3AF)
+val CineSearchBackground = Color(0xFF1A1C22)
