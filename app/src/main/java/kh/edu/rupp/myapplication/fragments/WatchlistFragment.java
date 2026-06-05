03295ca244505105e0bdf6a902909aba2831f8cd
macbook <seungvannet8@gmail.com>
2026-06-05 11:29:57 +0700

    Fix poster in fragment_home.xml, fragment_profile.xml, fragment_watchlist.xml, update poster and add poster .

--- app/src/main/java/kh/edu/rupp/myapplication/fragments/WatchlistFragment.java
+++ app/src/main/java/kh/edu/rupp/myapplication/fragments/WatchlistFragment.java
@@ -7,8 +7,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import kh.edu.rupp.myapplication.adapters.WatchlistAdapter;
+import kh.edu.rupp.myapplication.R;
 import kh.edu.rupp.myapplication.databinding.FragmentWatchlistBinding;
 import kh.edu.rupp.myapplication.models.Movie;
 import java.util.ArrayList;
@@ -28,26 +27,51 @@ public class WatchlistFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setupRecyclerView();
+        setupStaticWatchlist();
     }
 
-    private void setupRecyclerView() {
+    private void setupStaticWatchlist() {
         List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("Inception", "2010", "2h 28m", 0));
-        movies.add(new Movie("The Matrix", "1999", "2h 16m", 0));
-        movies.add(new Movie("Interstellar", "2014", "2h 49m", 0));
-        movies.add(new Movie("The Lord of the Rings", "2003", "3h 21m", 0));
+        movies.add(new Movie("Inception", "2010", "2h 28m", R.drawable.inception));
+        movies.add(new Movie("The Matrix", "1999", "2h 16m", R.drawable.thematrix));
+        movies.add(new Movie("Interstellar", "2014", "2h 49m", R.drawable.interstellar));
 
-        WatchlistAdapter adapter = new WatchlistAdapter(movies);
-        binding.rvWatchlist.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.rvWatchlist.setAdapter(adapter);
 
         if (movies.isEmpty()) {
             binding.emptyState.setVisibility(View.VISIBLE);
-            binding.rvWatchlist.setVisibility(View.GONE);
+            binding.cardMovieOne.setVisibility(View.GONE);
+            binding.cardMovieTwo.setVisibility(View.GONE);
+            binding.cardMovieThree.setVisibility(View.GONE);
+            return;
         } else {
             binding.emptyState.setVisibility(View.GONE);
-            binding.rvWatchlist.setVisibility(View.VISIBLE);
+        }
+
+        if (movies.size() > 0) {
+            Movie movie1 = movies.get(0);
+            binding.tvTitleOne.setText(movie1.getTitle());
+            binding.tvInfoOne.setText(movie1.getYear() + " • " + movie1.getDuration());
+            binding.ivPosterOne.setImageResource(movie1.getPosterResId());
+        } else {
+            binding.cardMovieOne.setVisibility(View.GONE);
+        }
+
+        if (movies.size() > 1) {
+            Movie movie2 = movies.get(1);
+            binding.tvTitleTwo.setText(movie2.getTitle());
+            binding.tvInfoTwo.setText(movie2.getYear() + " • " + movie2.getDuration());
+            binding.ivPosterTwo.setImageResource(movie2.getPosterResId());
+        } else {
+            binding.cardMovieTwo.setVisibility(View.GONE);
+        }
+
+        if (movies.size() > 2) {
+            Movie movie3 = movies.get(2);
+            binding.tvTitleThree.setText(movie3.getTitle());
+            binding.tvInfoThree.setText(movie3.getYear() + " • " + movie3.getDuration());
+            binding.ivPosterThree.setImageResource(movie3.getPosterResId());
+        } else {
+            binding.cardMovieThree.setVisibility(View.GONE);
         }
     }
 
